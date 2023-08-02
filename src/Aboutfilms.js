aebabc397a987833ccd0aa288152fae4271de8bb
dridi mohamed amine <dridimedamin@gmail.com>
2023-08-02 15:41:36 +0100

    first commit

---
+++ src/Aboutfilms.js
@@ -0,0 +1,38 @@
+import Chernobyl from "./Chernobyl.jpg";
+import fastandfurious from "./fastandfurious.jpg";
+import Thegodfather from "./Thegodfather.jpg";
+import thedarknight from "./thedarknight.jpg";
+import undisputed from "./undisputed.jpg";
+const Aboutfilms = [
+  {
+    image: Chernobyl,
+    title: "Chernobyl",
+    rating: 5,
+    description: "Reality",
+  },
+  {
+    image: fastandfurious,
+    title: "Fast and Furious",
+    rating: 3,
+    description: "Action",
+  },
+  {
+    image: Thegodfather,
+    title: "Thegodfather",
+    rating: 5,
+    description: "Action",
+  },
+  {
+    image: thedarknight,
+    title: "The dark night rises",
+    rating: 3,
+    description: "Fiction",
+  },
+  {
+    image: undisputed,
+    title: "undisputed",
+    rating: 2,
+    description: "Aventure",
+  },
+];
+export default Aboutfilms;
